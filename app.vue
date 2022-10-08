7def1c5376f7e4fc5c24f68fd7d48e4ad42d4625
Jia-Yin <jywglady@gmail.com>
2022-10-08 13:22:10 +0800

    Update

--- app.vue
+++ app.vue
@@ -1,25 +1,53 @@
 <template>
-  <div>
-    <button @click="page+=1">3</button>
-    <vue-pdf-embed :source="source1" :page='page'/>    
-  </div>
+    <div>
+        <button @click="page -= 1">Prev</button>
+        <button @click="page = 1">Home</button>
+        <button @click="page += 1">Next</button>
+        <button @click="goTelegram">Exit</button>
+        <vue-pdf-embed :source="source1" :page="page" />
+    </div>
 </template>
 
 <script>
-import VuePdfEmbed from 'vue-pdf-embed'
+import VuePdfEmbed from "vue-pdf-embed";
 
 // OR THE FOLLOWING IMPORT FOR VUE 2
 // import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
 
 export default {
-  components: {
-    VuePdfEmbed,
-  },
-  data() {
-    return {
-      source1: 'c1.pdf',
-      page: 1,
+    components: {
+        VuePdfEmbed,
+    },
+    head() {
+      return {
+        script: [{
+            src: "https://telegram.org/js/telegram-web-app.js"
+        }],
+      }
+    },
+    data() {
+        return {
+            source1: "c1.pdf",
+            page: 1,
+            done: false,
+        };
+    },
+    updated() {
+        // this.$nextTick(() => {
+        if (!this.done) {
+            console.log("Updated!")
+            window.Telegram.WebApp.ready();
+            window.Telegram.WebApp.expand();
+            this.done = true
+        }
+        // })
+    },
+    methods: {
+        goTelegram() {
+            window.Telegram.WebApp.sendData('OK');
+            window.Telegram.WebApp.close();                
+
+        },
     }
-  }
-}
+};
 </script>
