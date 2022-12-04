83fddf0e3c562f34ee084b1a4f699751897fd48c
Jia-Yin <jywglady@gmail.com>
2022-12-04 08:43:38 +0800

    Update

--- pages/test.vue
+++ pages/test.vue
@@ -41,7 +41,7 @@ export default {
     head() {
       return {
         script: [{
-            src: "https://telegram.org/js/telegram-web-app.js"
+            src: "https://telegram.org/js/telegram-web-app.js?1"
         }],
       }
     },
