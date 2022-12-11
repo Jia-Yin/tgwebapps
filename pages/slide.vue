f365d7cc65dbcd9dfe4a673d659fcaf0f6a5d88f
Jia-Yin <jywglady@gmail.com>
2022-12-11 21:54:34 +0800

    Update

--- pages/slide.vue
+++ pages/slide.vue
@@ -1,16 +1,23 @@
 <script setup>
 import VuePdfEmbed from "vue-pdf-embed"
-import { ref, onMounted } from "vue"
+import { ref, onMounted, computed } from "vue"
 const router = useRouter()
 let Script = document.createElement("script")
 Script.setAttribute("src", "https://telegram.org/js/telegram-web-app.js")
 document.head.insertBefore(Script, document.head.firstChild)
 // document.head.appendChild(Script);
 
+let lstr = window.location.hash.toString()
+const re = /query_id=(.*)&user/;
+const myArray = lstr.match(re);
+console.log("MA", myArray)
+const qid = myArray ? myArray[1] : "None"
+
 onMounted(() => {
     // router.go(0)
     setTimeout(() => {
         console.log("TG", window.Telegram)
+        console.log("QID", qid)
     }, 100)
     
 })
@@ -19,8 +26,7 @@ onMounted(() => {
 <template>
     <div>
         SLIDE TEST
-        <div ref="el">GGG</div>
-        <div ref="gg">KKK</div>
+        <div>{{qid}}</div>
     </div>
 </template>
 
