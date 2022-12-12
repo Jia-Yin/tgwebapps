303cb32a55ceacfb51d6af75c6b6c08e9bdbb32b
Jia-Yin <jywglady@gmail.com>
2022-12-12 15:19:53 +0800

    Update

--- pages/slide.vue
+++ pages/slide.vue
@@ -5,6 +5,7 @@ import { onMounted, ref } from "vue"
 const router = useRouter()
 const route = useRoute()
 const config = useRuntimeConfig()
+// WebApp
 let Script = document.createElement("script")
 Script.setAttribute("src", "https://telegram.org/js/telegram-web-app.js")
 document.head.insertBefore(Script, document.head.firstChild)
@@ -14,7 +15,7 @@ const re = /query_id=(.*)&user/;
 const myArray = lstr.match(re);
 const qid = myArray ? myArray[1] : "AAEQalw7AAAAABBqXDvw2SVX"
 const baseURL = config.app.baseURL
-const file = baseURL+(route.query.f ? route.query.f : 'c1.pdf')
+const pfile = baseURL+(route.query.f ? route.query.f : 'c1.pdf')
 let token = route.query.tk ? route.query.tk : 'MTA2Mjc4NzU4NyUzQUFBRVprcDdTLVFyT1BId1JtblR0YVdmYWVfOUZ3Rnd1M21R'
 try {
     let qstr = window.atob(token)
@@ -26,20 +27,33 @@ try {
 const tk = token
 
 // Slide
+const hmargin = 50
+const vmargin = 20
 const page = ref(1)
 const pages = ref(1)
-const height = ref(200)
-const width = ref(300)
+const pheight = ref(600)
+const pwidth = ref(300)
 const pdfRef = ref()
+const pdone = ref(false)
+const phmode = ref(false)
 function pdfRendered() {
-    pages.value = pdfRef.value.pageCount
-    resize()
+    if (!pdone.value) {
+        pages.value = pdfRef.value.pageCount
+        pdone.value = true
+    }
 }
-function resize() {
-    console.log("pdfRef", pdfRef.value)
-    console.log("Dimens", pdfRef.value.getPageDimensions())
-    width.value = window.innerWidth
-    height.value = window.innerHeight
+function rotate() {
+    pwidth.value = window.innerWidth
+    pheight.value = window.innerHeight
+    if (pwidth.value > pheight.value) {
+        console.log("P=TRUE")
+        phmode.value = true
+        window.Telegram.WebApp.MainButton.show()
+    } else {
+        console.log("P=FALSE")
+        phmode.value = false
+        window.Telegram.WebApp.MainButton.hide()
+    }
 }
 
 const goExit = (async () => {
@@ -59,25 +73,76 @@ onMounted(() => {
         window.Telegram.WebApp.MainButton.show()
         window.Telegram.WebApp.MainButton.setText('返回')
         window.Telegram.WebApp.MainButton.onClick(goExit)
-        window.addEventListener("orientationchange", resize)
-        window.addEventListener("resize", resize)
+        window.addEventListener("orientationchange", rotate)
+        pwidth.value = window.innerWidth
+        pheight.value = window.innerHeight
     }, 500)
 })
 </script>
 
 <template>
     <div>
-        <div class="vertical">
-            <vue-pdf-embed ref="pdfRef" :source="file" 
-                :page="page" :width="width-50" @rendered="pdfRendered"/>
-            <button @click="goExit">TEST</button>
+        <div v-if="pwidth > pheight" class="hmode">
+            <vue-pdf-embed :source="pfile"
+                :page="page" :height="pheight-hmargin"/>
+            <div align="center">
+                <div class="rbtns">
+                    <button class=vbtn @click="page=1">Begin</button>
+                    <button class=vbtn @click="page=(page>1)?page-1:page">Prev</button>
+                    <button class=vbtn @click="page=(page<pages)?page+1:page">Next</button>
+                    <button class=vbtn @click="page=pages">End</button>
+                </div>
+                <button class=ebtn @click="goExit">Exit</button>
+            </div>
+        </div>
+        <div v-else class="vmode">
+            <vue-pdf-embed ref="pdfRef" :source="pfile" :page="page" 
+                :width="pwidth-vmargin" @rendered="pdfRendered"/>
+            <div class="bbtns">
+                <button class=hbtn @click="page=1">Begin</button>
+                <button class=hbtn @click="page=(page>1)?page-1:page">Prev</button>
+                <button class=hbtn @click="page=(page<pageCount)?page+1:page">Next</button>
+                <button class=hbtn @click="goTelegram">Exit</button>
+            </div>
+            <p align="center">
+                <button class=ebtn @click="goExit">Exit</button>
+            </p>
         </div>
     </div>
 </template>
 
 
 <style scoped>
-.vertical {
-    margin: 18px 15px;
+.vmode {
+    height: 500px;
+    margin-left: 0px;
+    border: 1px solid green;
+}
+.hmode {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-around;    
+}
+.rbtns {
+    width: 100px;
+    z-index: 100;
+}
+.bbtns {
+    margin-top: 12px;
+    display: flex;
+    justify-content: space-around;    
+}
+.ebtn {
+    margin-top: 18px;
+    background: blue;
+    color: white;
+    width: 100px;
+}
+.hbtn {
+    width: 70px;
+}
+.vbtn {
+    margin: 12px auto;
+    width: 80px;
 }
 </style>
