27c2c6b303dc9166d7d7f69991ca64de5a8c28cf
Jia-Yin <jywglady@gmail.com>
2022-12-13 07:12:15 +0800

    working showmd

--- pages/showmd.vue
+++ pages/showmd.vue
@@ -5,6 +5,28 @@ import { ref, computed } from 'vue'
 import 'highlight.js/styles/github.css';
 const route = useRoute()
 
+// WebApp
+let Script = document.createElement("script")
+Script.setAttribute("src", "https://telegram.org/js/telegram-web-app.js")
+document.head.insertBefore(Script, document.head.firstChild)
+// document.head.appendChild(Script);
+let lstr = window.location.hash.toString()
+const re = /query_id=(.*)&user/;
+const myArray = lstr.match(re);
+const qid = myArray ? myArray[1] : "AAEQalw7AAAAABBqXDvw2SVX"
+const baseURL = config.app.baseURL
+const pfile = baseURL+(route.query.f ? route.query.f : 'c1.pdf')
+let token = route.query.tk ? route.query.tk : 'MTA2Mjc4NzU4NyUzQUFBRVprcDdTLVFyT1BId1JtblR0YVdmYWVfOUZ3Rnd1M21R'
+try {
+    let qstr = window.atob(token)
+    token = window.decodeURIComponent(qstr)
+} catch (e) {
+    token = ""
+    console.log("Error", e)
+}
+const tk = token
+
+// Marked Options
 marked.setOptions({
   highlight: function(code, lang) {
     return hljs.highlight(code, {language:lang, ignoreIllegals: true }).value;
